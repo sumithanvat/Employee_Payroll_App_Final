6e0910391aa4b611febe8e991a2791efd08982d0
Sumithanvat <skhanvat@gmail.com>
2023-06-18 19:16:40 +0530

    Added:NAme and salary listener

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,112 +1,84 @@
 class EmployeePayrollData {
-   
-    get name(){
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
         return this._name;
     }
 
-    set name(name){
+    set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-         if(nameRegex.test(name))
-         this._name = name;
-         else
-         throw "Name is Incorrect";
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Name is Incorrect";
     }
-    get profileImage(){
+
+    get profileImage() {
         return this._profileImage;
     }
 
-    set profileImage(profileImage){
+    set profileImage(profileImage) {
         this._profileImage = profileImage;
-         }
+    }
 
-    get gender(){
+    get gender() {
         return this._gender;
     }
-    set gender(gender){
-        this._gender = gender;
-         }
 
-    get department(){
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
         return this._department;
     }
 
-    set department(department){
+    set department(department) {
         this._department = department;
     }
-    get salary(){
+
+    get salary() {
         return this._salary;
     }
-    set salary(salary){
-        this._salary = salary
-      }
-    get startDate(){
+
+    set salary(salary) {
+        this._salary = salary;
+
+    }
+
+    get startDate() {
         return this._startDate;
     }
-    set startDate(startDate){
+
+    set startDate(startDate) {
         let difference = Date.now() - startDate;
         difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
-        if(difference > 30 || difference < 0){
-            throw "Start Date is Invalid";
+        if (difference > 30 || difference < 0) {
+          throw "Start Date is Invalid";
+        } else {
+          this._startDate = startDate;
         }
-        else{
-            this._startDate = startDate;
-        }
-        
     }
-    get notes(){
+
+    get notes() {
         return this._notes;
     }
-    set notes(notes){
+
+    set notes(notes) {
         this._notes = notes;
     }
-    
-    toString(){
-        const option = {year: 'numeric', month: 'long', day: 'numeric'};
-        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", option);
-        return "Name =" + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
     }
 }
-
-    function  save() {
-        let employeeName = document.querySelector('#name').value;
-        let profileList = document.querySelectorAll('input[name="profile"]');
-        let emploeeProfileImage;
-        for(let image of profileList){
-            if(image.checked){
-                emploeeProfileImage = image.value;
-                break;
-            }
-        }
-
-        let genderList = document.querySelectorAll('input[name="gender"]');
-        let employeeGender;
-        for(let gender of genderList){
-            if(gender.checked){
-                employeeGender = gender.value;
-                break;
-            }
-        }
-        let department = document.querySelectorAll('.checkbox:checked');
-        let employeeDepartment = new Array();
-        for(let department of departmentList){
-            if(department.checked){
-                employeeDepartment.push(department.value);
-            }
-        }
-
-        let employeeSalary = document.querySelector('#salary').value;
-
-        let day = document.querySelector('#day').value;
-        let month = document.querySelector('#month').value;
-        let year = document.querySelector('#year').value;
-        let employeeStartDate = new Date(year, month, day);
-
-        let employeeNotes = document.querySelector('#notes').value;
-
-        try{
-            let EmployeePayrollData = new EmployeePayrollData(employeeName, emploeeProfileImage,employeeGender, employeeDepartment, employeeSalary, employeeStartDate, employeeNotes);
-            console.log(EmployeePayrollData.toString());
-        } catch (e){
-            console.error(e)
-        }
-        }
